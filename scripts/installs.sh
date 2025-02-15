1a5c88a19fa0b20ce797acaaa7a02ffc823d8ce2
andi klein <pabloemma@gmail.com>
2025-02-15 15:58:29 -0700

    more work on moving

--- scripts/installs.sh
+++ scripts/installs.sh
@@ -6,6 +6,7 @@ sudo apt install python3-matplotlib
 sudo apt install python3-numpy
 sudo apt install pyhton3-pandas
 sudo apt-get install -y libxcb-cursor-dev
+sudo apt install psql
 
 pip3 install pyside6 --break-system-packages
 
